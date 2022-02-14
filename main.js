f260cf2515e211cb2dd9b80054a4a356d01c8bc5
Emily Florence Starr <emilyflo.starr@gmail.com>
2022-02-13 21:54:26 -0500

    styled and good to go with the exception of not pausing audio when other audio starts

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ searchBox.addEventListener('submit', function(e) {
             <img src="${result.artworkUrl100}">
             <p>${result.artistName}</p>
             <p>${result.trackName}</p>
+            <p><em>Genre</em>: ${result.primaryGenreName}</p>
             <audio controls src="${result.previewUrl}"></audio>
             `
             outputDiv.appendChild(artistCard)
@@ -32,30 +33,3 @@ searchBox.addEventListener('submit', function(e) {
     }
     )
 })
-        // document.querySelector('#searchBox').innerHTML = `<p>${results.artistName}</p>`
-    //     document.querySelector('#user-name').innerHTML = `<h2>${data.name}</h2>`
-    // console.log(data.repos_url)
-
-    // for (let result of data) {
-    //     document.querySelector(
-    //       '#search-box'
-    //     ).innerHTML += `<p><a href=${repo.url}>${repo.name}</a></p>`
-    // // return data.repos_url 
-// function validateForm(){
-//     let input=document.querySelector('')
-// }
-
-// connect search to fetch from the API a band name or artist name. The artist or band name must be in template literal to call any search term, i think.
-
-// `${artistName}`
-//make search dynamic for different results
-//grab audio
-
-// When the user types their search term and presses the submit button (or presses Enter), make the search request to the API.
-// https://itunes.apple.com/search?term=jack+johnson&limit=15
-// When the API returns a response, use the results to display a listing of songs related to the search term.
-// When a user clicks a song in your listing, the song should play in an <audio> tag that you've also added to the page (see the mockup).
-// the results we're looking for are image, song title, artist/band, <audio play/stop> which should be at the top of the page
-// myAudioElement.addEventListener("canplaythrough")
-// parameterkeyvalue = (``)
-// <script src="https://"
